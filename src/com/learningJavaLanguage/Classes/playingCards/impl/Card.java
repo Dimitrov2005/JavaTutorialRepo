6eca410627a94cd232ba72873b38c0ff065f1401
dimitrov2005 <s.dimitrov2005@gmail.com>
2021-02-12 16:00:09 +0200

    Inheritance

--- src/com/learningJavaLanguage/Classes/playingCards/impl/Card.java
+++ src/com/learningJavaLanguage/Classes/playingCards/impl/Card.java
@@ -2,10 +2,11 @@ package com.learningJavaLanguage.Classes.playingCards.impl;
 
 import com.learningJavaLanguage.Classes.playingCards.api.BasicCard;
 
+import java.util.Objects;
+
 public class Card implements BasicCard {
     private String rank;
     private String suit;
-
     /**
      * Class whose instances represent a single playing card from a deck of cards.
      * Playing cards have two distinguishing properties: rank and suit.
@@ -25,4 +26,28 @@ public class Card implements BasicCard {
     public String getSuit() {
         return suit;
     }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return Objects.equals(rank, card.rank) &&
+                Objects.equals(suit, card.suit);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rank, suit);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "rank='" + rank + '\'' +
+                ", suit='" + suit + '\'' +
+                '}';
+    }
+
 }
